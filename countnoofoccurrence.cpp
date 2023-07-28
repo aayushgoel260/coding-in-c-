cce4c6a08258b2c336913269b156905f56d97ac6
Aayush Goel <122432016+aayushgoel260@users.noreply.github.com>
2023-07-28 10:35:36 +0530

    Add files via upload

---
+++ countnoofoccurrence.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v(6);
+    for(int i=0;i<v.size();i++){
+        cin>>v[i];
+    }
+    cout<<"Enter x : ";
+    int x;
+    cin>>x;
+    int occurrences=0;
+    for(int ele:v){
+        if(ele==x){
+            occurrences++;
+        }
+    }
+    cout<<occurrences<<endl;
+    return 0;
+}
